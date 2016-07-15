1516fc879a2723b98e890ecbce5045dcac820332
liam38 <cjp9103@naver.com>
2016-07-15 21:26:27 +0900

    "Initializing memory above 1MB(0x100000)"

--- MINT64/01.Kernel32/Source/Main.c
+++ MINT64/01.Kernel32/Source/Main.c
@@ -1,9 +1,17 @@
 #include "Types.h"
 
 void kPrintString(int iX, int iY, const char* pcString);
+BOOL kInitializeKernel64Area(void);
 
 void Main(void) {
+	DWORD i;
+
 	kPrintString(0, 3, "C Language Kernel Started..");
+
+	//IA-32e 모드의 커널 영역을 초기화
+	kInitializeKernel64Area();
+	kPrintString(0, 4, "IA-32e Kernel Area Initialization Complete");
+
 	while(1);
 }
 
@@ -16,3 +24,20 @@ void kPrintString(int iX, int iY, const char* pcString) {
 		pstScreen[index].bCharacter = pcString[index];
 }
 
+BOOL kInitializeKernel64Area(void) {
+	DWORD* pdwCurrentAddress;
+
+	//초기화를 시작할 어드레스인 0x100000(1MB)을 설정
+	pdwCurrentAddress = (DWORD*)0x100000;
+
+	//마지막 어드레스인 0x600000(6MB)까지 loop 돌면서 4byte씩 0으로 채움
+	while((DWORD)pdwCurrentAddress < 0x600000) {
+		*pdwCurrentAddress = 0x00;
+
+		if(*pdwCurrentAddress != 0)
+			return FALSE;
+		pdwCurrentAddress++;
+	}
+
+	return TRUE;
+}
